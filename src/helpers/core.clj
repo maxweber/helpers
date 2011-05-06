fd98db7849c1d15a6b19e5f2100f11ae791c103a
Maximilian Weber <weber.maximilian@googlemail.com>
2011-05-06 15:39:05 +0200

    Better doc-string for helpers.core/symbol-map

--- src/helpers/core.clj
+++ src/helpers/core.clj
@@ -3,8 +3,8 @@
 
 (defmacro symbol-map
   "Takes any number of (quoted) symbols and builds a hash map with key value pairs
-   where the key is the keyword of the symbol name and the value the symbol's value
-   in the lexical scope where symbol map is invoked. An example:
+   where the key is a keyword of the symbol's name and the value the symbol's value
+   in the lexical scope where symbol-map is invoked. An example:
 
    >(let [a \"hello\" b \"world\"] (symbol-map 'a 'b))
    {:a \"hello\", :b \"world\"}"
