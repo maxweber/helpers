fd5d4f611ab79c37107ce5ec477779b0fb730422
Maximilian Weber <weber.maximilian@googlemail.com>
2011-05-27 17:25:31 +0200

    A regex pattern for a UUID has been added.

--- src/helpers/uuid.clj
+++ src/helpers/uuid.clj
@@ -5,3 +5,6 @@
   "Returns a new UUID as string."
   []
   (.toString (java.util.UUID/randomUUID)))
+
+(def uuid-pattern
+     #"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
