cefc83a5aac01dfedc9e528567de46b20dabcaf4
Maximilian Weber <weber.maximilian@googlemail.com>
2011-06-29 10:16:05 +0200

    The symbol-map function has been improved, so that it is not necessary any more to quote the symbols.

--- src/helpers/core.clj
+++ src/helpers/core.clj
@@ -2,11 +2,13 @@
   helpers.core)
 
 (defmacro symbol-map
-  "Takes any number of (quoted) symbols and builds a hash map with key value pairs
-   where the key is a keyword of the symbol's name and the value the symbol's value
-   in the lexical scope where symbol-map is invoked. An example:
+  "Takes any number of symbols and builds a hash map with key value pairs
+   where the key is a keyword of the symbol's name and the value is the
+   symbol's value in the lexical scope where symbol-map is invoked.
 
-   >(let [a \"hello\" b \"world\"] (symbol-map 'a 'b))
+   An example:
+
+   >(let [a \"hello\" b \"world\"] (symbol-map a b))
    {:a \"hello\", :b \"world\"}"
   [& symbols]
-  (into {} (map (fn [s] (let [sym s] `[(keyword ~sym) ~(second sym)])) symbols)))
+  (into {} (map (fn [s] `[(keyword (quote ~s)) ~s]) symbols)))
